f32251cc4bfa90f524a4a05f1254c355155846ae
Romeo Gono <romeogono1@aol.com>
2018-09-13 00:23:46 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,11 +5,5 @@ class App < Sinatra::Base
 	get '/' do
 		"<h1>Hello World</h1>"
 	end
-	
-	get '/description' do
-		"<h1>Welcome to my first Sinatra App ever!!!</h1>"
-		"<p>I'll see you on this page</p>"
-
-	end
 
 end
